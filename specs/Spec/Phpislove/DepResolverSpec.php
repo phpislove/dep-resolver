4b198afa6a0029662dfbdab2259ae6ecb06be42e
Ilya S <public.ilya.s@gmail.com>
2014-10-05 23:19:49 +0700

    add check for nonexistent class

--- specs/Spec/Phpislove/DepResolverSpec.php
+++ specs/Spec/Phpislove/DepResolverSpec.php
@@ -1,13 +1,20 @@
 <?php namespace Spec\Phpislove;
 
 use PhpSpec\ObjectBehavior;
-// use Prophecy\Argument;
 
 class DepResolverSpec extends ObjectBehavior {
 
     function it_is_initializable()
     {
         $this->shouldHaveType('Phpislove\DepResolver');
+    }
+
+    function it_throws_an_exception_if_class_does_not_exist()
+    {
+        $nonexistentClass = uniqid().'_class';
+
+        $this->shouldThrow('Phpislove\Exceptions\NonexistentClass')
+             ->duringResolve($nonexistentClass);
     }
 
 }
--- src/Phpislove/DepResolver.php
+++ src/Phpislove/DepResolver.php
@@ -2,6 +2,16 @@
 
 class DepResolver {
 
-
+    /**
+     * @param string $class
+     * @return mixed
+     */
+    public function resolve($class)
+    {
+        if ( ! class_exists($class))
+        {
+            throw new Exceptions\NonexistentClass;
+        }
+    }
 
 }
